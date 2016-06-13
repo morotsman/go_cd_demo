ade469d2c9effc69cc294eac7fbe4dfcb3ab41a6
morotsman <leopold.niklas@gmail.com>
2016-06-13 08:47:31 +0200

    Update rakefile.rb

--- rakefile.rb
+++ rakefile.rb
@@ -1,4 +1,25 @@
 desc 'Build the app'
+
+task :unit_test do
+  puts "Running the unit tests!"
+end
+
 task :build do
-  puts "Hello World!"
+  puts "Building the app!"
+end
+
+task :run_cucumber_tests do
+  puts "Running the tests!"
+end
+
+task :run_selenium_tests do
+  puts "Running the selenium test"
+end
+
+task :deploy_to_acceptance_test do
+  puts "Deploy to Acceptance test"
+end
+
+task :deploy_to_production do
+  puts "Deploy to production!"
 end
